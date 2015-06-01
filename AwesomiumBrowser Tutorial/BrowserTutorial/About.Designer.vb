7435a079708b4e8501bfe55216540a75c1198a53
Titancraftz <patrickkelly5401@gmail.com>
2015-06-01 19:52:55 -0400

    Init. Upload

---
+++ AwesomiumBrowser Tutorial/BrowserTutorial/About.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class About
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(About))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.Image = Global.BrowserTutorial.My.Resources.Resources.AirBrowse_Technologies_logo
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 13)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(387, 99)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Light", 15.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(13, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(387, 79)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Awesomium Browser" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Demo Version)"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Light", 10.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(13, 198)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(387, 73)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Visit us at: " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://airbrowsetechnologies.weebly.com/"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'About
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(412, 280)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "About"
+        Me.Text = "About"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ AwesomiumBrowser Tutorial/BrowserTutorial/Forms/PageInfoDialog.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Windows.Forms
+Imports Awesomium.Core
+
+Public Class PageInfoDialog
+
+    Public Sub New(pagetitle As String, pageurl As String, pageinfo As WebPageInfo, historycount As Integer, img As Image)
+        InitializeComponent()
+
+        title.Text = pagetitle
+        url.Text = pageurl
+
+        Select Case pageinfo.SecurityStatus
+            Case SecurityStatus.Authenticated
+                securitystat.Text = "This page is securly connected with HTTPS and its identity has been verified by " + pageinfo.CertIssuer + "."
+            Case SecurityStatus.AuthenticationBroken
+                securitystat.Text = "This page is supposed to be secure, but there were errors in HTTPS transfer."
+            Case SecurityStatus.Unauthenticated
+                securitystat.Text = "This page has not been authenticated using HTTPS."
+            Case SecurityStatus.Unknown
+                securitystat.Text = "The identity of this page is unknown!"
+
+        End Select
+
+        Select Case pageinfo.ContentStatus
+            Case ContentStatusFlags.Normal
+                contentstat.Text = "This page contains no insecure content."
+            Case ContentStatusFlags.DisplayedInsecureContent
+                contentstat.Text = "This page is connected with HTTPS but has displayed HTTP resources."
+            Case ContentStatusFlags.RanInsecureContent
+                contentstat.Text = "This page is connected with HTTPS but has ran HTTP scripts. Be careful!"
+        End Select
+
+        favicon.Image = img
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
